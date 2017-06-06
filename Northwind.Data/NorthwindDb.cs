d4f37ee9e1ec884a95483e80590c69c1b00823db
Immo Landwerth <immol@microsoft.com>
2017-06-05 22:20:39 -0700

    Add .NET Standard and web app

---
+++ Northwind.Data/NorthwindDb.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Data;
+
+namespace Northwind
+{
+    public class NorthwindDb
+    {
+        public static string GetData()
+        {
+            var result = "";
+            var dataSet = new DataSet();
+            dataSet.ReadXml(@"C:\demos\northwind.xml");
+
+            var employeeTable = dataSet.Tables["Employees"];
+
+            foreach (DataRow row in employeeTable.Rows)
+            {
+                var firstName = Convert.ToString(row["FirstName"]);
+                var lastName = Convert.ToString(row["LastName"]);
+                var birthdate = Convert.ToDateTime(row["Birthdate"]);
+                var retirementDate = birthdate.AddYears(65);
+
+                var isRetired = retirementDate < DateTime.Now;
+                if (!isRetired)
+                    continue;
+
+                result += $"{firstName} {lastName}: {retirementDate}\r\n";
+            }
+
+            return result;
+        }
+    }
+}
